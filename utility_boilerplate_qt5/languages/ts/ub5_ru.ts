1a62745126c664b4b84654bf01607c946c3cbf0e
thelongrunsmoke <thelongrunsmoke@gmail.com>
2021-06-01 20:42:15 +0500

    Prepare russian translation file

---
+++ utility_boilerplate_qt5/languages/ts/ub5_ru.ts
@@ -0,0 +1,218 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>UtilityMainWindow</name>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="72"/>
+        <source>&amp;File</source>
+        <translation>&amp;Файл</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="73"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="76"/>
+        <source>&amp;Edit</source>
+        <translation>&amp;Правка</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="77"/>
+        <source>Edit</source>
+        <translation>Правка</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="80"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Справка</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="93"/>
+        <source>&amp;New</source>
+        <translation>&amp;Новый</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="94"/>
+        <source>Create a new file</source>
+        <translation>Создать новый документ.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="101"/>
+        <source>&amp;Open</source>
+        <translation>&amp;Открыть</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="102"/>
+        <source>Open file</source>
+        <translation>Открыть существующий документ.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="109"/>
+        <source>&amp;Save</source>
+        <translation>&amp;Сохранить</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="110"/>
+        <source>Save file</source>
+        <translation>Сохранить текущий документ.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="117"/>
+        <source>Save as…</source>
+        <translation>Сохранить как…</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="118"/>
+        <source>Save file as…</source>
+        <translation>Сохранить текущий файл как…</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="122"/>
+        <source>R&amp;ecent files…</source>
+        <translation>Недавние &amp;файлы…</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="126"/>
+        <source>E&amp;xit</source>
+        <translation>В&amp;ыход</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="127"/>
+        <source>Quit</source>
+        <translation>Выход</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="137"/>
+        <source>Cu&amp;t</source>
+        <translation>Выре&amp;зать</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="138"/>
+        <source>Cut the current selection&apos;s contents to the clipboard</source>
+        <translation>Вырезать текущее выделение в буфер обмена.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="145"/>
+        <source>&amp;Copy</source>
+        <translation>&amp;Копировать</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="146"/>
+        <source>Copy the current selection&apos;s contents to the clipboard</source>
+        <translation>Копировать текущее выделение в буфер обмена.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="153"/>
+        <source>&amp;Paste</source>
+        <translation>&amp;Вставить</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="154"/>
+        <source>Paste the clipboard&apos;s contents into the current selection</source>
+        <translation>Вставить содержимое из буфера обмена.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="165"/>
+        <source>Se&amp;ttings</source>
+        <translation>&amp;Параметры</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="166"/>
+        <source>Show application settings.</source>
+        <translation>Показать настройки приложения.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="174"/>
+        <source>&amp;About</source>
+        <translation>&amp;О приложении</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="175"/>
+        <source>About application.</source>
+        <translation>Показать информацию о приложении.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="179"/>
+        <source>About &amp;Qt</source>
+        <translation>О &amp;Qt</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="180"/>
+        <source>Show the Qt library&apos;s About box</source>
+        <translation>Показать информацию о библиотеке Qt.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="191"/>
+        <source>&amp;%1 %2</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="204"/>
+        <source>All files (*.*)</source>
+        <translation>Все файлы (*.*)</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="218"/>
+        <source>Error</source>
+        <translation>Ошибка</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="219"/>
+        <source>Cannot open file %1 %2</source>
+        <translation>Невозможно открыть файл %1 %2</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="297"/>
+        <source>About Utility Boilerplate Qt</source>
+        <translation>О каркасе Utility Boilerplate Qt</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="298"/>
+        <source>This is example shows how to crate simple utility application using Utility Boilerplate Qt5. For details see
+&lt;a href=&apos;https://github.com/TheLongRunSmoke/utility-boilerplate-qt&apos;&gt;https://github.com/TheLongRunSmoke/utility-boilerplate-qt&lt;/a&gt;</source>
+        <translation>Это пример простого приложения созданого с использование каркаса using Utility Boilerplate Qt5. Подробная информация доступна по сслыке
+&lt;a href=&apos;https://github.com/TheLongRunSmoke/utility-boilerplate-qt&apos;&gt;https://github.com/TheLongRunSmoke/utility-boilerplate-qt&lt;/a&gt;</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="309"/>
+        <location filename="../../utilitymainwindow.cpp" line="317"/>
+        <location filename="../../utilitymainwindow.cpp" line="325"/>
+        <source>Warning</source>
+        <translation>Предупреждение</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="310"/>
+        <source>Not implemented yet. Override UtilityMainWindow::cut() slot before using.</source>
+        <translation>Перед использованием переопределите слот UtilityMainWindow::cut() в вашей реализации.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="318"/>
+        <source>Not implemented yet. Override UtilityMainWindow::copy() slot before using.</source>
+        <translation>Перед использованием переопределите слот UtilityMainWindow::copy() в вашей реализации.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="326"/>
+        <source>Not implemented yet. Override UtilityMainWindow::paste() slot before using.</source>
+        <translation>Перед использованием переопределите слот UtilityMainWindow::paste() в вашей реализации.</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="408"/>
+        <source>Application</source>
+        <translation>Приложение</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="409"/>
+        <source>The document has been modified.
+Do you want to save your changes?</source>
+        <translation>Документ был изменён.
+Сохранить изменения?</translation>
+    </message>
+    <message>
+        <location filename="../../utilitymainwindow.cpp" line="434"/>
+        <source>Ready</source>
+        <translation>Готово.</translation>
+    </message>
+</context>
+</TS>
