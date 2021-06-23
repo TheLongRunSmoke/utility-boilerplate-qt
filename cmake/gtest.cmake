97122521e91d74d288caacfad254271a0fdd79f3
thelongrunsmoke <thelongrunsmoke@gmail.com>
2021-06-23 22:13:13 +0500

    Now test coverage work

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -9,7 +9,7 @@
 # Let's start with including fetcher, to make possible to load code from external source.
 include(FetchContent)
 # Load Google Test source code. Use hash of HEAD commit on master branch.
-FetchContent_Declare(googletest URL https://github.com/google/googletest/archive/a3460d1aeeaa43fdf137a6adefef10ba0b59fe4b.zip)
+FetchContent_Declare(googletest URL https://github.com/google/googletest/archive/355d57d90d9744c41ac7c99f1e960778f1c63040.zip)
 # Prevent overriding the parent project's compiler/linker settings.
 set(gtest_force_shared_crt
     ON
@@ -17,5 +17,4 @@ set(gtest_force_shared_crt
 )
 # Include library to build environment and enable CTest support.
 FetchContent_MakeAvailable(googletest)
-enable_testing()
 include(GoogleTest)
--- src/example/tests/test.cpp
+++ src/example/tests/test.cpp
@@ -5,3 +5,13 @@
 TEST(Smoke, CheckEnvironment) {  // NOLINT(cert-err58-cpp)
     ASSERT_EQ(MainWindow::objectId(), QString::number(1000, 10));
 }
+
+TEST(MainWindow, loadFile) {
+    QApplication* app{};
+    char** argv{};
+    int arg = 0;
+    app = new QApplication(arg, argv);
+    MainWindow window;
+    window.loadFile("settings.ini");
+    QApplication::exit(0);
+}
